22c1d4668060689687fdfa9cb581a4611cb4fb1a
Rohit Rajendra Barate <rohitbarate100@gmail.com>
2023-08-15 11:50:08 +0530

    wiss image added

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const userRoute = require('./routes/userRoute')
-const noteRoute = require('./routes/noteRoute')
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const userRoute = require("./routes/userRoute");
+const noteRoute = require("./routes/noteRoute");
 
 const app = express();
 
@@ -12,18 +12,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // testing
-app.get('/',(req,res)=>{
-    res
-    .send("everything is perfect...!")
-    // .download('index.js')
-})
+app.get("/wistatussaver/shareappimage", (req, res) => {
+  res.status(200).send("success").download("./images/shareApp.png");
+});
 
 // base url endpoint
 // https://{web url}/data/v1
 
 // Routes
-app.use('/data/v1/user',userRoute)
-app.use('/data/v1/note',noteRoute)
-
+app.use("/data/v1/user", userRoute);
+app.use("/data/v1/note", noteRoute);
 
 module.exports = app;
